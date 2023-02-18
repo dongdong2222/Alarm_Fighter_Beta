e5b228bde6ff3d645a1f9006fab0c73c7f306ff9
동주 장 <ehdwngudwn@naver.com>
2023-02-18 15:10:07 +0900

    feature : attack ver1

--- Assets/Scripts/Items/Weapons/Sword.cs
+++ Assets/Scripts/Items/Weapons/Sword.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Sword : Weapon
 {
+    GameObject go;
     public Sword()
     {
         weaponObject = Managers.Resource.Load<GameObject>("Prefabs/Items/Weapons/Sword");
+        attackEffect = "Prefabs/Effect/Weapons/Sword";
+
         Rank = Define.ItemRank.Normal;
     }
+
+    public override void Attack(Transform parent)
+    {
+        go = Managers.Resource.Instantiate(attackEffect, parent);
+
+    }
+
+    //IEnumerator Destroy()
+    //{
+    //    yield return new WaitForSeconds(1f);
+    //    Managers.Resource.Destroy(go);
+    //}
     // Start is called before the first frame update
 }
